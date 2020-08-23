9da0046c87080fb566814b59024edb4b706fa7c9
takaseko <takaseko@takasekouheinoMacBook-Pro.local>
2020-08-23 14:11:10 +0900

    hackathon_メソッド定義

---
+++ hackathon.rb
@@ -0,0 +1,12 @@
+def time
+    puts "今日はどの項目を何分勉強した？？"
+    time = gets.to_f
+    print "学習項目を入力>"
+    print "学習時間を入力>"
+    while true
+    puts  "学習記録"
+    break;
+    end
+end
+
+time
---
+++ method.rb
@@ -0,0 +1,8 @@
+def study
+  puts "今日はどの項目を何分勉強した？？"
+  time = gets.to_f
+  print "学習項目を入力>"
+  print "学習時間を入力>"
+end
+
+study
